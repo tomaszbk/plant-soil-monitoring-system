be11917e168ce606b6bf3e65152433625f92a82f
tomaszbk-pc <zubik.tomas@gmail.com>
2023-11-12 12:17:38 -0300

    feat: :sparkles: updated server main.py and added pyproject.toml

--- server/main.py
+++ server/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
+from datetime import datetime
+from pymongo import MongoClient
+from server.config import config
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+    yield
+    app.mongodb_client.close()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+app = FastAPI(lifespan=lifespan)
+    
+
+@app.post("/humidity")
+async def root(humidity: float, request: Request):
+    now = datetime.now()
+    request.app.database.humiditydb.insert_one({'date': now, 'humidity': humidity})
+    return {'date': now, 'humidity': humidity}
